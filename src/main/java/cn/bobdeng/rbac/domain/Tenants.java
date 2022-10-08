7ee0731f833831a8d39a266e2c6691811e2dbeb4
bobdeng <bobdeng@163.com>
2022-10-08 14:57:27 +0800

    feat: find tenant by id

--- src/main/java/cn/bobdeng/rbac/domain/Tenants.java
+++ src/main/java/cn/bobdeng/rbac/domain/Tenants.java
@@ -1,6 +1,8 @@
 package cn.bobdeng.rbac.domain;
 
-public class Tenants{
+import java.util.Optional;
+
+public class Tenants {
     private TenantRepository tenantRepository;
 
     public Tenants(TenantRepository tenantRepository) {
@@ -10,5 +12,9 @@ public class Tenants{
 
     public Tenant add(TenantDescription tenantDescription) {
         return this.tenantRepository.save(new Tenant(tenantDescription));
+    }
+
+    private Optional<Tenant> findById(Integer id) {
+        return this.tenantRepository.findByIdentity(id);
     }
 }
